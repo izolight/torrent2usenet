18fafcd152c29da5b7b5fe195830161c0abc0d3b
Gabor Tanz <izolight@gmail.com>
2015-05-13 13:33:23 +0200

    Switch to deluge watchfolder and update matchings.

--- mag2tor.sh
+++ mag2tor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [[ "$1" =~ xt=urn:btih:([^&/]+) ]] || exit;
-echo "d10:magnet-uri${#1}:${1}e" > "/home/***REMOVED***/downloads/watch/meta-${BASH_REMATCH[1]}.torrent"
+echo "d10:magnet-uri${#1}:${1}e" > "/home/***REMOVED***/.watch/meta-${BASH_REMATCH[1]}.torrent"
